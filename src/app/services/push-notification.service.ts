a28f6f3d9b132fe5ef1549b840f61ee9ff5f8057
ifloresr7 <151829949+ifloresr7@users.noreply.github.com>
2024-05-13 23:19:33 +0200

    Se hacen cambios importantes en la implementacion.

--- src/app/services/push-notification.service.ts
+++ src/app/services/push-notification.service.ts
@@ -1,31 +1,11 @@
-/*import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken } from 'firebase/messaging';
-
-export class PushNotificationService {
-  constructor() {
-    const firebaseConfig = {
-      apiKey: 'AIzaSyArTWjHtnAW17b9__JAlzGl4Y_rAg4v-ck',
-      authDomain: 'frontmovil-producto4-d3941.firebaseapp.com',
-      projectId: 'frontmovil-producto4-d3941',
-      storageBucket: 'frontmovil-producto4-d3941.appspot.com',
-      messagingSenderId: '349167493430',
-      appId: '1:349167493430:web:5b058e264d4ad522d41397',
-      measurementId: 'G-RCF84KNFGF',
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-
-    // Initialize Firebase Cloud Messaging and get a reference to the service
-    const messaging = getMessaging(app);
-
-  }
-}*/
 import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken } from 'firebase/messaging';
+import { collection, getFirestore, addDoc, getDocs, where, query } from "firebase/firestore";
 
 export class PushNotificationService {
-  private messaging: any; // O puedes tipar esta variable con el tipo de Firebase Messaging correspondiente
+  private messaging: any;
+  private firestore: any;
+  private isPermissionGranted: boolean = false;
 
   constructor() {
     const firebaseConfig = {
@@ -37,88 +17,66 @@ export class PushNotificationService {
       appId: '1:349167493430:web:5b058e264d4ad522d41397',
       measurementId: 'G-RCF84KNFGF',
     };
-
-    // Inicializa Firebase
     const app = initializeApp(firebaseConfig);
-
-    // Inicializa Firebase Cloud Messaging y obtén una referencia al servicio
     this.messaging = getMessaging(app);
-
-    // Solicita permiso de notificación al usuario
+    this.firestore = getFirestore(app); // Initialize Firestore
     this.requestPermission();
-
-    // Obtiene el token de registro
     this.getRegistrationToken();
   }
 
-  requestPermission = () => {
+  async requestPermission(): Promise<void> {
     console.log('Solicitando permiso...');
-    /* Notification.requestPermission().then(
-      (permission: NotificationPermission) => {
-        if (permission === 'granted') {
-          console.log('Permiso de notificación concedido.');
-        } else {
-          console.log('El usuario no ha concedido el permiso de notificación.');
-        }
+    if (!('Notification' in window)) {
+      throw new Error('La API de notificaciones no está soportada en este navegador');
+    }
+
+    try {
+      const permission = await Notification.requestPermission();
+      if (permission === 'granted') {
+        console.log('Permiso concedido..');
+      } else {
+        throw new Error('Los permisos de notificación fueron denegados');
       }
-    );*/
+    } catch (error) {
+      throw new Error('Error al obtener el token de Firebase: ' + error);
+    }
+  }
 
-    /*  this.requestPermission = () => {
-      return new Promise(async (resolve, reject) => {
-        const permsis = await Notification.requestPermission();
-        if (permsis === "granted") {
-          const tokenFirebase = await this.messaging.getToken();
-          resolve(tokenFirebase);
-
-        } else {
-          reject(new Error("No se otorgaron los permisos"))
-        }
-      })
-    } 
-  }*/
-    return new Promise(async (resolve, reject) => {
-      console.log('Solicitando permiso2...');
-      if (!('Notification' in window)) {
-        reject(
-          new Error(
-            'La API de notificaciones no está soportada en este navegador'
-          )
-        );
-        return;
-      }
-
-      try {
-        const permission = await Notification.requestPermission();
-        if (permission === 'granted') {
-          //const tokenFirebase = await this.messaging.getToken();
-          //resolve(tokenFirebase);
-          console.log('Permiso concedido..');
-        } else {
-          reject(new Error('Los permisos de notificación fueron denegados'));
-        }
-      } catch (error) {
-        reject(new Error('Error al obtener el token de Firebase: ' + error));
-      }
-    });
-  };
-
-  getRegistrationToken(): void {
+  async getRegistrationToken(): Promise<void> {
     console.log('Pidiendo token');
     getToken(this.messaging, {
       vapidKey: '',
     })
-      .then((currentToken: string | null) => {
+      .then(async (currentToken: string | null) => {
         if (currentToken) {
           console.log('Token de registro:', currentToken);
-          // Enviar el token al servidor y actualizar la interfaz de usuario si es necesario
+          const existsInFirestore = await this.checkTokenInFirestore(currentToken);
+          if (existsInFirestore) {
+            console.log('El token ya está registrado en Firestore.');
+          } else {
+            console.log('El token no está registrado en Firestore. Registrándolo...');
+            const docData = {
+              token: currentToken
+            }
+            await addDoc(collection(this.firestore, "tokens"), docData);
+          }
         } else {
-          console.log(
-            'No hay disponible un token de registro. Solicitar permiso para generar uno.'
-          );
+          console.log('No hay disponible un token de registro. Solicitar permiso para generar uno.');
         }
       })
       .catch((err: any) => {
         console.error('Ocurrió un error al obtener el token de registro:', err);
       });
+  }
+
+  async checkTokenInFirestore(token: string): Promise<boolean> {
+    try {
+      const tokensCollectionRef = collection(this.firestore, 'tokens');
+      const querySnapshot = await getDocs(query(tokensCollectionRef, where('token', '==', token)));
+      return !querySnapshot.empty;
+    } catch (error) {
+      console.error('Error al verificar el token en Firestore:', error);
+      return false;
+    }
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,5 @@
 export const environment = {
   firebaseConfig: {
-/*    piKey: "AIzaSyBS0H9GKnHd_x2l9qzhjje4CBR7QkbJ6qI",
-    authDomain: "frontmovil-producto2.firebaseapp.com",
-    projectId: "frontmovil-producto2",
-    storageBucket: "frontmovil-producto2.appspot.com",
-    messagingSenderId: "976282756700",
-    appId: "1:976282756700:web:c033d28cf049c2195ee899",
-    measurementId: "G-VH05Z6SF7Z"*/
     apiKey: "AIzaSyArTWjHtnAW17b9__JAlzGl4Y_rAg4v-ck",
     authDomain: "frontmovil-producto4-d3941.firebaseapp.com",
     projectId: "frontmovil-producto4-d3941",
